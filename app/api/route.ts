288d8be2667d722bf48487f1cc4fbd1a9be28cc7
Sam Selikoff <sam.selikoff@gmail.com>
2025-07-02 15:05:15 -0400

    domain

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,4 +1,5 @@
 export async function GET() {
   await new Promise((resolve) => setTimeout(resolve, 1_000));
+
   return Response.json({ data: Math.random() });
 }
--- app/lib/fetch-data.ts
+++ app/lib/fetch-data.ts
@@ -1,7 +1,8 @@
 import invariant from 'tiny-invariant';
+import { domain } from './domain';
 
 export async function fetchRandomNumber(query = 'foo') {
-  const res = await fetch(`http://localhost:3000/api?${query}`);
+  const res = await fetch(`${domain}/api?${query}`);
   const { data } = await res.json();
 
   invariant(typeof data === 'number');
